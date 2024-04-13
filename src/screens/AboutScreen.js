30838e94f83f8223b0c4039eda2514ddcb05aa74
DMould123 <95115610+DMould123@users.noreply.github.com>
2024-04-14 01:22:52 +0200

    Added quiz image and custom start button on HomeScreen and Updated ContactScreen with some simple styling to expand on

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,9 +1,9 @@
-import { Button, View, Text } from 'react-native'
+import { Button, View, Text, StyleSheet } from 'react-native'
 
 export default function AboutScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>About Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>About Screen</Text>
       <Button
         title="Go back to Home"
         onPress={() => navigation.navigate('Home')}
@@ -11,3 +11,17 @@ export default function AboutScreen({ navigation }) {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff'
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20
+  }
+})
