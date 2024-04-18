e6327e6fef16b6e8eaa40f11ea3e4ed10010e9f6
DMould123 <95115610+DMould123@users.noreply.github.com>
2024-04-18 23:04:06 +0200

    Enhanced styling and added an improvised  hover effect for social media icons

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, Button, StyleSheet, Linking, Image } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 const AboutScreen = ({ navigation }) => {
   const portfolioUrl = 'https://david-mould-portfolio-page.netlify.app'
+  const [hoveredIcon, setHoveredIcon] = useState(null)
 
   const handleVisitWebsite = () => {
     Linking.openURL(portfolioUrl)
@@ -27,32 +28,38 @@ const AboutScreen = ({ navigation }) => {
       <View style={styles.socialLinks}>
         <Icon
           name="linkedin"
-          size={30}
-          color="#000"
+          size={36} // Increased icon size
+          color={hoveredIcon === 'linkedin' ? '#2867B2' : '#000'}
           onPress={() => {
             Linking.openURL(
               'https://www.linkedin.com/in/david-mould-b6731a21a/'
             )
           }}
           style={styles.icon}
+          onMouseEnter={() => setHoveredIcon('linkedin')}
+          onMouseLeave={() => setHoveredIcon(null)}
         />
         <Icon
           name="github"
-          size={30}
-          color="#000"
+          size={36}
+          color={hoveredIcon === 'github' ? '#211F1F' : '#000'}
           onPress={() => {
             Linking.openURL('https://github.com/DMould123')
           }}
           style={styles.icon}
+          onMouseEnter={() => setHoveredIcon('github')}
+          onMouseLeave={() => setHoveredIcon(null)}
         />
         <Icon
           name="twitter"
-          size={30}
-          color="#000"
+          size={36}
+          color={hoveredIcon === 'twitter' ? '#1DA1F2' : '#000'}
           onPress={() => {
             Linking.openURL('https://twitter.com/DM12_51')
           }}
           style={styles.icon}
+          onMouseEnter={() => setHoveredIcon('twitter')}
+          onMouseLeave={() => setHoveredIcon(null)}
         />
       </View>
       <Button
@@ -70,15 +77,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#fff',
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
+    paddingVertical: 30
   },
   title: {
-    fontSize: 24,
+    fontSize: 28,
     fontWeight: 'bold',
     marginBottom: 20
   },
   description: {
-    fontSize: 16,
+    fontSize: 18,
     marginBottom: 20,
     textAlign: 'center'
   },
@@ -90,19 +98,18 @@ const styles = StyleSheet.create({
   },
   socialLinks: {
     flexDirection: 'row',
-    justifyContent: 'center',
-    marginBottom: 20
+    justifyContent: 'space-evenly'
   },
   icon: {
     marginHorizontal: 10
   },
   button: {
     backgroundColor: '#007bff',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-    marginTop: 10,
-    width: '100%'
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+    marginTop: 20,
+    width: '80%'
   }
 })
 
