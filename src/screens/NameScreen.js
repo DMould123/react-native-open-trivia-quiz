9f14cd5d7f229ad7818eb9e126747004d3a5f0f8
DMould123 <95115610+DMould123@users.noreply.github.com>
2024-04-27 20:05:16 +0200

    Added a gradient to the NameScreen with expo-linear-gradient

--- src/screens/NameScreen.js
+++ src/screens/NameScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
 
 const NameScreen = ({ navigation }) => {
   const [inputName, setInputName] = useState('')
@@ -14,13 +15,26 @@ const NameScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Enter your name:</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => setInputName(text)}
-        value={inputName}
+      <LinearGradient
+        colors={['#77A1D3', '#79CBCA', '#E684AE']}
+        style={styles.background}
       />
-      <Button title="Next" onPress={handleSubmit} />
+      <View style={styles.content}>
+        <Text style={styles.title}>Enter your name:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => setInputName(text)}
+          value={inputName}
+          placeholder="Your Name"
+          placeholderTextColor="#ccc"
+        />
+        <LinearGradient
+          colors={['#4c669f', '#3b5998', '#192f6a']}
+          style={styles.button}
+        >
+          <Button title="Next" color="#fff" onPress={handleSubmit} />
+        </LinearGradient>
+      </View>
     </View>
   )
 }
@@ -30,21 +44,43 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'orange'
+  },
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: '100%'
+  },
+  content: {
+    width: '80%',
+    alignItems: 'center',
+    justifyContent: 'center',
     paddingHorizontal: 20,
-    backgroundColor: '#fff'
+    paddingVertical: 30,
+    borderRadius: 10
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20
+    marginBottom: 20,
+    color: '#fff'
   },
   input: {
     height: 40,
     width: '100%',
-    borderColor: 'gray',
+    borderColor: '#999',
     borderWidth: 1,
     marginBottom: 20,
-    paddingHorizontal: 10
+    paddingHorizontal: 10,
+    color: '#000'
+  },
+  button: {
+    padding: 15,
+    width: '100%',
+    alignItems: 'center',
+    borderRadius: 5
   }
 })
 
