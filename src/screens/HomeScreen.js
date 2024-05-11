825ff6642bdcf023c9905ffa2ab8e15fb4534af4
DMould123 <95115610+DMould123@users.noreply.github.com>
2024-05-11 10:06:46 +0200

    Added an icon to assets. Added a rotating image on HomeScreen and icon also

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,20 +1,58 @@
-import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native'
+import React, { useEffect, useRef } from 'react'
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Animated,
+  Easing
+} from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 const HomeScreen = ({ navigation }) => {
   const handleStartQuiz = () => {
     navigation.navigate('Quiz', { screen: 'NameScreen' })
   }
 
+  // Animation setup
+  const spinValue = useRef(new Animated.Value(0)).current
+  const spinDuration = 2000
+  const pauseDuration = 2500
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(spinValue, {
+          toValue: 1,
+          duration: spinDuration,
+          easing: Easing.linear,
+          useNativeDriver: true
+        }),
+        Animated.delay(pauseDuration)
+      ])
+    ).start()
+  }, [])
+
+  const spin = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg']
+  })
+
   return (
     <View style={styles.container}>
-      <Image
+      <Animated.Image
         source={require('../../assets/quiz.jpg')}
-        style={styles.image}
+        style={[styles.image, { transform: [{ rotate: spin }] }]}
         resizeMode="cover"
       />
       <Text style={styles.title}>Welcome to the Quiz App!</Text>
+      <Text style={styles.description}>
+        Ready to challenge yourself with our exciting quizzes? Explore a variety
+        of topics, improve your knowledge, and enjoy the thrill of learning!
+      </Text>
       <TouchableOpacity style={styles.button} onPress={handleStartQuiz}>
         <Text style={styles.buttonText}>Start Quiz</Text>
+        <Icon name="arrow-right" size={20} color="#fff" style={styles.icon} />
       </TouchableOpacity>
     </View>
   )
@@ -30,7 +68,13 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20
+    marginBottom: 10,
+    textAlign: 'center'
+  },
+  description: {
+    fontSize: 16,
+    marginBottom: 20,
+    textAlign: 'center'
   },
   image: {
     width: '75%',
@@ -39,13 +83,19 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: '#007bff',
+    flexDirection: 'row',
+    alignItems: 'center',
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5
   },
   buttonText: {
     color: '#fff',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    marginRight: 5
+  },
+  icon: {
+    marginLeft: 5
   }
 })
 
